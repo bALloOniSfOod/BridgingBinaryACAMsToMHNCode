c4de56bc8cfd4bee48d6f766b2518e54fdf976b6
Ryan Cerauli <ryan.cerauli@gmail.com>
2026-07-10 17:18:30 -0400

    Create shapeColorSize.py with shape data
    Added a list of shape, color, and size permutations.

---
+++ SupplementaryCode/shapeColorSize.py
@@ -0,0 +1,25 @@
+# All permutations of a set of shapes, colors, and sizes in a list of lists
+
+
+shapeColorSize = [["shape", "color", "size"], 
+ ["square", "red", 10], 
+ ["circle", "blue", 20], 
+ ["triangle", "green", 30], 
+ ["diamond", "yellow", 40], 
+ ["hexagon", "purple", 50], 
+ ["heptagon", "indigo", 10], 
+ ["line", "violet", 20], 
+ ["point", "turquoise", 30], 
+ ["cube", "lavender", 40], 
+ ["tetrahedron", "gold", 50], 
+ ["crescent", "red", 10], 
+ ["rectangle", "blue", 20], 
+ ["tube", "green", 30], 
+ ["cylinder", "yellow", 40], 
+ ["sphere", "purple", 50], 
+ ["prism", "indigo", 10], 
+ ["octagon", "violet", 20], 
+ ["nonagon", "turquoise", 30], 
+ ["mobiusstrip", "lavender", 40], 
+ ["boomerang", "gold", 50], ]
+ 
